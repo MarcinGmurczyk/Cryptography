e72fdd0a7f1b7d843acb95961125d67563c1af4e
MarcinGmurczyk <mgmuras@gmail.com>
2015-04-17 18:37:06 +0200

    adding missing files

---
+++ Cryptography.Tests/CryptographyCiphersTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Cryptography;
+
+namespace Cryptography.Tests
+{
+    class CryptographyCiphersTests
+    {
+        [Test, TestCaseSource(typeof(CryptographyTestsData), "plainText")]
+        public void AffineTest(string plainText)
+        {
+            var test = new AffineCipher(new AffineCipherKey());
+            Assert.AreEqual(plainText, test.decrypt(test.encrypt(plainText)));
+            test = new AffineCipher(new AffineCipherKey(23, 88));
+            Assert.AreEqual(plainText, test.decrypt(test.encrypt(plainText)));
+        }
+
+        [Test, TestCaseSource(typeof(CryptographyTestsData), "plainText")]
+        public void XORCipherTest(string plainText)
+        {
+            var cipher = new XORCipher((short)Cryptography._rand.Next(1, 100));
+            Assert.AreEqual(plainText, cipher.decrypt(cipher.encrypt(plainText)));
+            cipher = new XORCipher();
+            Assert.AreEqual(plainText, cipher.decrypt(cipher.encrypt(plainText)));
+        }
+
+        [Test, TestCaseSource(typeof(CryptographyTestsData), "plainText")]
+        public void RSACipherTest(string plainText)
+        {
+            var cipher = new RSA(new RSAKey(Cryptography.GenerateRandomPrimeNumber128b(), Cryptography.GenerateRandomPrimeNumber128b()));
+            Assert.AreEqual(plainText, cipher.decrypt(cipher.encrypt(plainText)));
+            cipher = new RSA(new RSAKey());
+            Assert.AreEqual(plainText, cipher.decrypt(cipher.encrypt(plainText)));
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void RSACipherException()
+        {
+            new RSA(new RSAKey(12, 7));
+        }
+    }
+}
